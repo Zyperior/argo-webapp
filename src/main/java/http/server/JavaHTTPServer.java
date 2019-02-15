c7d3d500d1dbeea161ec753d45e95b8a175835fe
Robin S <45070726+wuxxerno@users.noreply.github.com>
2019-02-15 09:59:18 +0000

    add null handling for stream and tokenizer

--- src/main/java/http/server/JavaHTTPServer.java
+++ src/main/java/http/server/JavaHTTPServer.java
@@ -4,71 +4,75 @@ import java.io.*;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
-public class JavaHTTPServer implements Runnable{
+public class JavaHTTPServer implements Runnable {
 
-    private Socket connect;
+	private Socket connect;
 
-    public JavaHTTPServer(Socket c) {
-        connect = c;
-    }
+	public JavaHTTPServer(Socket c) {
+		connect = c;
+	}
 
-    public void run() {
+	public void run() {
 
-        BufferedReader in = null;
-        PrintWriter out = null;
-        BufferedOutputStream dataOut = null;
-        RequestType type = RequestType.INVALID;
-        String fileRequested;
+		BufferedReader in = null;
+		PrintWriter out = null;
+		BufferedOutputStream dataOut = null;
+		RequestType type = RequestType.INVALID;
+		String fileRequested = "";
+		String method = "";
+		try {
 
-        try {
+			in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+			out = new PrintWriter(connect.getOutputStream());
+			dataOut = new BufferedOutputStream(connect.getOutputStream());
 
-            in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-            out = new PrintWriter(connect.getOutputStream());
-            dataOut = new BufferedOutputStream(connect.getOutputStream());
+			String input = in.readLine();
 
+			// Dont parse nulls!
+			if (input != null) {
+				StringTokenizer parse = new StringTokenizer(input);
 
-            String input = in.readLine();
-            StringTokenizer parse = new StringTokenizer(input);
-            String method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
+				// Dont request more tokens than we have!
+				if (parse.hasMoreTokens()) {
+					method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
 
-            fileRequested = parse.nextToken().toLowerCase();
+					fileRequested = parse.nextToken().toLowerCase();
+				}
+			}
 
-            if(method.equals("GET")){
-                type = RequestType.GET;
-            }
-            else if(method.equals("HEAD")){
-                type = RequestType.HEAD;
-            }
-            else if(method.equals("POST")){
-                type = RequestType.POST;
-            }
+			if (method.equals("GET")) {
+				type = RequestType.GET;
+			} else if (method.equals("HEAD")) {
+				type = RequestType.HEAD;
+			} else if (method.equals("POST")) {
+				type = RequestType.POST;
+			}
 
-            new HTTPRequest(type,fileRequested,out,dataOut);
+			new HTTPRequest(type, fileRequested, out, dataOut);
 
+		} catch (IOException ioe) {
 
-        } catch (IOException ioe) {
+			System.err.println("Server error : " + ioe);
 
-            System.err.println("Server error : " + ioe);
+		} finally {
 
-        } finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+				if (out != null) {
+					out.close();
+				}
+				if (dataOut != null) {
+					dataOut.close();
+				}
+				connect.close(); // we close socket connection
+			} catch (Exception e) {
+				System.err.println("Error closing stream : " + e.getMessage());
+			}
 
-            try {
-                if (in != null) {
-                    in.close();
-                }
-                if (out != null) {
-                    out.close();
-                }
-                if (dataOut != null) {
-                    dataOut.close();
-                }
-                connect.close(); // we close socket connection
-            } catch (Exception e) {
-                System.err.println("Error closing stream : " + e.getMessage());
-            }
+		}
 
-        }
-
-    }
+	}
 
 }
