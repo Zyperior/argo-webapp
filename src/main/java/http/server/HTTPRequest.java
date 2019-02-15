5f904a65b8c9b6d710f959e32d2b0bba59a5fdc3
Robin S <45070726+wuxxerno@users.noreply.github.com>
2019-02-15 10:02:05 +0000

    add functionality to retrieve params from GET request, sys.out debug msg shows url and params

--- src/main/java/http/server/HTTPRequest.java
+++ src/main/java/http/server/HTTPRequest.java
@@ -5,133 +5,153 @@ import java.util.Date;
 
 class HTTPRequest {
 
-    private static final File WEB_ROOT = new File(".");
-    private static final String DEFAULT_FILE = "index.html";
-    private static final String FILE_NOT_FOUND = "404.html";
-    private static final String METHOD_NOT_SUPPORTED = "501.html";
+	private static final File WEB_ROOT = new File(".");
+	private static final String DEFAULT_FILE = "index.html";
+	private static final String FILE_NOT_FOUND = "404.html";
+	private static final String METHOD_NOT_SUPPORTED = "501.html";
 
-    private File file;
-    private int fileLength;
-    private RequestType type;
-    private String fileRequested;
-    private String content;
-    private PrintWriter out;
-    private BufferedOutputStream dataOut;
+	private File file;
+	private int fileLength;
+	private RequestType type;
+	private String fileRequested;
+	private String params;
+	private String content;
+	private PrintWriter out;
+	private BufferedOutputStream dataOut;
 
-    HTTPRequest(RequestType type, String fileRequested, PrintWriter out, BufferedOutputStream dataOut) throws IOException {
+	HTTPRequest(RequestType type, String fileRequested, PrintWriter out, BufferedOutputStream dataOut)
+			throws IOException {
+		// check and retrieve params if they exist
+		try {
+			if (fileRequested.contains("?")) {
+				String[] parts = fileRequested.split("\\?");
 
-        this.type = type;
-        this.out = out;
-        this.dataOut = dataOut;
-        this.fileRequested = fileRequested;
-        try{
-            this.file = getRequestedFile();
-        }
-        catch(FileNotFoundException e){
-            fileNotFound();
-        }
+				this.fileRequested = parts[0];
+				this.params = parts[1];
+			} else {
+				this.fileRequested = fileRequested;
+			}
 
-        this.fileLength = (int)file.length();
+		} catch (ArrayIndexOutOfBoundsException e) {
 
-        switch(type){
-            case GET:
-                get();
-                break;
-            case HEAD:
-                head();
-                break;
-            case POST:
-                post();
-                break;
-            case INVALID:
-                invalid();
-        }
+		}
 
-    }
+		this.type = type;
+		this.out = out;
+		this.dataOut = dataOut;
 
-    private void get() throws IOException {
-        out.println("HTTP/1.1 200 OK");
-        standardOutToClient();
-    }
+		// print out the url and params
+		System.out.println(this.fileRequested);
+		System.out.println(this.params);
 
-    private void head(){
+		try {
+			this.file = getRequestedFile();
+		} catch (FileNotFoundException e) {
+			fileNotFound();
+		}
 
-    }
+		this.fileLength = (int) file.length();
 
-    private void post(){
+		switch (type) {
+		case GET:
+			get();
+			break;
+		case HEAD:
+			head();
+			break;
+		case POST:
+			post();
+			break;
+		case INVALID:
+			invalid();
+		}
 
-    }
+	}
 
-    private void invalid() throws IOException {
-        out.println("HTTP/1.1 501 Not Implemented");
-        standardOutToClient();
-    }
+	private void get() throws IOException {
+		out.println("HTTP/1.1 200 OK");
+		standardOutToClient();
+	}
 
-    private void fileNotFound() throws IOException {
-        out.println("HTTP/1.1 404 File Not Found");
-        standardOutToClient();
+	private void head() {
 
-    }
+	}
 
-    private void standardOutToClient() throws IOException {
-        byte[] fileData = readFileData(fileLength);
+	private void post() {
 
-        out.println("Server: Java HTTP Server from SSaurel : 1.0");
-        out.println("Date: " + new Date());
-        out.println("Content-type: " + content);
-        out.println("Content-length: " + fileLength);
-        out.println(); // blank line between headers and content, very important !
-        out.flush(); // flush character output stream buffer
-        dataOut.write(fileData, 0, fileLength);
-        dataOut.flush();
-    }
+	}
 
-    private File getRequestedFile() throws FileNotFoundException{
+	private void invalid() throws IOException {
+		out.println("HTTP/1.1 501 Not Implemented");
+		standardOutToClient();
+	}
 
-        File file;
+	private void fileNotFound() throws IOException {
+		out.println("HTTP/1.1 404 File Not Found");
+		standardOutToClient();
 
-        if (fileRequested.endsWith("/")) {
-            fileRequested += DEFAULT_FILE;
-        }
+	}
 
-        if(type==RequestType.INVALID){
-            fileRequested = METHOD_NOT_SUPPORTED;
-        }
+	private void standardOutToClient() throws IOException {
+		byte[] fileData = readFileData(fileLength);
 
-        file = new File(WEB_ROOT, fileRequested);
+		out.println("Server: Java HTTP Server from SSaurel : 1.0");
+		out.println("Date: " + new Date());
+		out.println("Content-type: " + content);
+		out.println("Content-length: " + fileLength);
+		out.println(); // blank line between headers and content, very important !
+		out.flush(); // flush character output stream buffer
+		dataOut.write(fileData, 0, fileLength);
+		dataOut.flush();
+		out.close();
+	}
 
-        if(!file.exists()){
-            fileRequested = FILE_NOT_FOUND;
-            this.file = new File(WEB_ROOT, fileRequested);
-            throw new FileNotFoundException();
-        }
+	private File getRequestedFile() throws FileNotFoundException {
 
-        content = getContentType(fileRequested);
-        return file;
+		File file;
 
-    }
+		if (fileRequested.endsWith("/")) {
+			fileRequested += DEFAULT_FILE;
+		}
 
-    private String getContentType(String fileRequested) {
-        if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
-            return "text/html";
-        else
-            return "text/plain";
-    }
+		if (type == RequestType.INVALID) {
+			fileRequested = METHOD_NOT_SUPPORTED;
+		}
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private byte[] readFileData(int fileLength) throws IOException {
-        FileInputStream fileIn = null;
-        byte[] fileData = new byte[fileLength];
+		file = new File(WEB_ROOT, fileRequested);
 
-        try {
-            fileIn = new FileInputStream(file);
-            fileIn.read(fileData);
-        } finally {
-            if (fileIn != null) {
-                fileIn.close();
-            }
-        }
+		if (!file.exists()) {
+			fileRequested = FILE_NOT_FOUND;
+			this.file = new File(WEB_ROOT, fileRequested);
+			throw new FileNotFoundException();
+		}
 
-        return fileData;
-    }
+		content = getContentType(fileRequested);
+		return file;
+
+	}
+
+	private String getContentType(String fileRequested) {
+		if (fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
+			return "text/html";
+		else
+			return "text/plain";
+	}
+
+	@SuppressWarnings("ResultOfMethodCallIgnored")
+	private byte[] readFileData(int fileLength) throws IOException {
+		FileInputStream fileIn = null;
+		byte[] fileData = new byte[fileLength];
+
+		try {
+			fileIn = new FileInputStream(file);
+			fileIn.read(fileData);
+		} finally {
+			if (fileIn != null) {
+				fileIn.close();
+			}
+		}
+
+		return fileData;
+	}
 }
