cf622449ff402c1c6ed6d663cefe4817fc867f17
Wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2019-02-23 18:36:06 +0000

    add optional support for server config file

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,28 +2,53 @@ package main;
 
 import http.server.JavaHTTPServer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
-
+import java.util.Properties;
 
 public class Main {
 
-    private static final int PORT = 8080;
+	private static int PORT = 8080;
 
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverConnect = new ServerSocket(PORT);
-            System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+	public static void main(String[] args) {
+		try {
 
-            while (true) {
-                JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+			if (new File("config.xml").isFile() == true) {
+				Properties serverSettings = new Properties();
 
-                Thread thread = new Thread(myServer);
-                thread.start();
-            }
+				FileInputStream in = new FileInputStream("config.xml");
 
-        } catch (IOException e) {
-            System.err.println("Server Connection error : " + e.getMessage());
-        }
-    }
+				serverSettings.loadFromXML(in);
+				String hold = serverSettings.getProperty("server_port");
+				try {
+					PORT = Integer.parseInt(hold);
+					if (PORT < 8000 || PORT > 8089) {
+						PORT = 8080;
+					}
+				} catch (Exception e) {
+
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			ServerSocket serverConnect = new ServerSocket(PORT);
+			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+
+			while (true) {
+				JavaHTTPServer myServer = new JavaHTTPServer(serverConnect.accept());
+
+				Thread thread = new Thread(myServer);
+				thread.start();
+			}
+
+		} catch (IOException e) {
+			System.err.println("Server Connection error : " + e.getMessage());
+		}
+	}
 }
